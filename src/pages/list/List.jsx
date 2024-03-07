a558ed1381bf28e2b71fadbf9b5d8d15adbebcfa
fia <fia@192.168.1.46>
2024-03-08 02:29:26 +0700

    hello

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -6,13 +6,16 @@ import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
 import SearchItem from "../../components/searchItem/SearchItem";
-
+import useFetch from "../../hooks/useFetch";
 const List = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
+
+
+  const { data, loading, error , reFetch} = useFetch(`http://localhost:8800/api/hotels?city=${destination}`)
 
   return (
     <div>
@@ -87,15 +90,11 @@ const List = () => {
             <button>Search</button>
           </div>
           <div className="listResult">
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
+            {loading? "loading": <>
+            {data.map(item => (            
+              <SearchItem item={item} key={item._id}/>
+              ))}
+            </>}
           </div>
         </div>
       </div>
