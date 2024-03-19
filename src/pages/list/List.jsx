8dc2200fa41efc3607b9ab9f1e9bef3d1189cd49
fia <fiakantapon@gmail.com>
2024-03-19 10:47:54 +0700

    aw

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -13,10 +13,16 @@ const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
+  const [min, setMin] = useState(undefined)
+  const [max, setMax] = useState(undefined)
 
 
-  const { data, loading, error , reFetch} = useFetch(`http://localhost:8800/api/hotels?city=${destination}`)
+  const { data, loading, error , reFetch} = useFetch(`http://localhost:8800/api/hotels?city=${destination}&min=${min || 0}&max=${max || 99999}`);
 
+  const handleClick = ( ) => {
+    reFetch();
+  }
+ 
   return (
     <div>
       <Navbar />
@@ -50,13 +56,13 @@ const List = () => {
                   <span className="lsOptionText">
                     Min price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input onChange={ e => setMin(e.target.value)} type="number" className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">
                     Max price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input onChange={ e => setMax(e.target.value)} type="number" className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Adult</span>
@@ -87,14 +93,14 @@ const List = () => {
                 </div>
               </div>
             </div>
-            <button>Search</button>
+            <button onClick={handleClick}>Search</button>
           </div>
           <div className="listResult">
-            {loading? "loading": <>
+            <>
             {data.map(item => (            
               <SearchItem item={item} key={item._id}/>
               ))}
-            </>}
+            </>
           </div>
         </div>
       </div>
